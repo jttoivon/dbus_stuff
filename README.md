a31fbf76f44866847cc072e51a2a4f7c5b1b2862
Jarkko Toivonen <jttoivon@cs.helsinki.fi>
2022-03-04 19:56:39 +0200

    Fixed the printing of the tree. Added some examples.

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a small program to make queries about Dbus services, object paths and services. Although D-feet is a great program for browsing the Dbus tree, it doesn't for example allow finding all services that offer an interface.
+
+To see a description of the command line options call with ```./query.py -h```.
+
+Without any parameters it lists the available services. By default the used bus is the system bus. Use the ```--session``` option to change that.
+
+    ./query.py
+
+To search only for services in the org.freedesktop namespace you can use 
+    ./query.py --service 'org.freedesktop.*'
+
+Similarly, you can pass --object-path and --interface option the define glob patterns for these. If no options or just the --service option is given, then only services will be printed. If --object-path option is given, then both service name and object path is printed. Finally, if --interface option is given, then all three are printed: service, object path and interface. If the filter pattern for higher levels in the hierarchy are not given but a lower one is, they are assumed to be '*'. The following example demonstrates this.
+
+Find all services and object paths that implement the ObjectManager interface:
+
+    ./query.py --interface org.freedesktop.DBus.ObjectManager
+
+By default the output is in tree form. The next example requests for the output as a table.
+Show service and object path pairs that implement the Bluetooth profile interface:
+
+    ./query.py --table --all --interface org.bluez.Profile1  --pid --command-line
+
+The --all option above asks to print also don't services that don't have a well known name. This can sometimes cause timeout problems, since some services don't reply to the introspect method calls. The timeout on my machine seems to be 25+25 seconds. Which makes scanning for object path for all services very slow. I don't yet know how to fix this.
--- query.py
+++ query.py
@@ -8,6 +8,13 @@
 # Ideas from the following page were used:
 # https://unix.stackexchange.com/questions/203410/how-to-list-all-object-paths-under-a-dbus-service
 
+
+# Note in the program the variable depth doesn't refer to the depth of the objects hierarchy.
+# Instead the meaning is the following
+# depth==1 is the service level
+# depth==2 is the object path level
+# depth==3 is the interface level
+
 import sys
 import argparse
 import fnmatch
@@ -19,20 +26,23 @@ import dbus.mainloop.glib
 from collections import defaultdict
 from xml.etree import ElementTree
 
-parser = argparse.ArgumentParser(description='query dbus tree')
+parser = argparse.ArgumentParser(description='Query dbus tree')
 #parser.add_argument('-s','--system',  nargs='?', help='query SystemBus (default)', required=False, default=True)
 #parser.add_argument('-S','--session', nargs='?', help='query SessionBus',          required=False, default=False)
 parser.add_argument('-t','--table',  action='store_true', default=False,  help='Show results as a table instead of a tree')
 parser.add_argument('-p','--pid',  action='store_true', default=False,  help='Show the process id that offers the service')
 parser.add_argument('-c','--command-line',  action='store_true', default=False,  help='Show the command line of the process that offers the service')
 parser.add_argument('-a','--all',  action='store_true', default=False,  help='Show all services, not only well-known service')
-parser.add_argument('--system',  action='store_true', default=True,  help='query SystemBus (default)')
-parser.add_argument('--session', action='store_true', default=False, help='query SessionBus')
+parser.add_argument('--system',  action='store_true', default=True,  help='Query SystemBus (default)')
+parser.add_argument('--session', action='store_true', default=False, help='Query SessionBus')
 parser.add_argument('-d', '--debug', action='store_true', default=False, help='Print debug information')
 parser.add_argument('-s','--service', help='search pattern', required=False, default="*")
 parser.add_argument('-o','--object-path', help='search pattern', required=False, default=None)
 parser.add_argument('-i','--interface', help='search pattern', required=False, default=None)
 args = vars(parser.parse_args())
+
+debug = args["debug"]
+
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 #loop = gobject.MainLoop()
@@ -46,15 +56,17 @@ else:
     #print("Using system bus")
     bus = dbus.SystemBus()
 
+myname = bus.get_unique_name()
+if debug:
+    print("My name is %s" % myname) 
 #sys.exit(1)
 driver = dbus.Interface(bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus'), 'org.freedesktop.DBus')
 connections = driver.ListNames()
 # pid_to_count = {}
 # pid_to_connections = defaultdict(list)
 
-debug = args["debug"]
 if debug:
-    print("Got the following command line args: %s" ,args["command_line"])
+    print("Got the following command line args: %s" % args["command_line"])
 if args["table"]:
     tree=False
 else:
@@ -98,12 +110,25 @@ if "command_line" in args:
 if not tree:
     print("\t".join(columns))
 
-def rec_intro(bus, service, object_path, cursor):
+def print_service_pid_command(cursor, sep=" "):
+    print(cursor["service"], end="")
+    if args["pid"]:
+        print("%s%i" % (sep, cursor["pid"]), end="")
+    if args["command_line"]:
+        print("%s%s" % (sep, cursor["command_line"]), end="")
+    print()
+
+
+def rec_intro(bus, service, object_path, cursor, service_printed=False):
+    object_printed = False
     #print("In rec_intro")
     skip_object = not fnmatch.fnmatch(object_path, args["object_path"])
     #if skip_object:
     #    print("Skipping object %s" % object_path)
-    if tree and not skip_object:
+    if tree and not skip_object and depth == 2:
+        if not service_printed:
+            print_service_pid_command(cursor)
+            service_printed=True
         print("\t%s" % object_path)
     obj = bus.get_object(service, object_path)
     iface = dbus.Interface(obj, 'org.freedesktop.DBus.Introspectable')
@@ -113,8 +138,9 @@ def rec_intro(bus, service, object_path, cursor):
         print("Got an exception when trying introspection")
         return
     subobjects = []
+    # Iterate over interfaces and subobjects
     for child in ElementTree.fromstring(xml_string):
-        if child.tag == 'node':
+        if child.tag == 'node':   # Found a new subobject. Add it to the list
             if object_path == '/':
                 object_path = ''
             new_path = '/'.join((object_path, child.attrib['name']))
@@ -126,10 +152,16 @@ def rec_intro(bus, service, object_path, cursor):
             #    print("Skipping interface %s" % interface)
             if tree:
                 if depth == 3 and not skip_interface:
+                    if not service_printed:
+                        print_service_pid_command(cursor)
+                        service_printed=True
+                    if not object_printed:
+                        print("\t%s" % object_path)
+                        object_printed=True
                     print("\t\t%s" % interface)
             elif not skip_interface:
                 print("%s\t%s" % (service, object_path), end="")
-                if depth == 3 and not skip_interface:
+                if depth == 3:
                     print("\t%s" % interface, end="")
                 if args["pid"]:
                     print("\t%s" % cursor["pid"], end="")
@@ -137,12 +169,14 @@ def rec_intro(bus, service, object_path, cursor):
                     print("\t%s" % cursor["command_line"], end="")
                 print()
     for new_path in subobjects:
-        rec_intro(bus, service, new_path, cursor)
+        rec_intro(bus, service, new_path, cursor, service_printed)
 
 
 for service in connections:
     if not args["all"] and service.startswith(':'): continue
     if not fnmatch.fnmatch(service, args["service"]):
+        continue
+    if service == myname:  # Don't query about ourself
         continue
     #print(args["command_line"])
     try:
@@ -153,25 +187,15 @@ for service in connections:
             #print("here")
             cursor["command_line"] = get_command_line(cursor["pid"])
         if tree:
-            print(service, end="")
-            if args["pid"]:
-                print("\t%i" % cursor["pid"], end="")
-            if args["command_line"]:
-                print("\t%s" % cursor["command_line"], end="")
-            print()
+            if depth == 1:
+                print_service_pid_command(cursor)
             if depth > 1:
                 rec_intro(bus, service, "/", cursor)
         else:
-            if depth > 1:
-                rec_intro(bus, service, "/", cursor)
+            if depth == 1:
+                print_service_pid_command(cursor, sep="\t")
             else:
-                print(service, end="")
-                if args["pid"]:
-                    print("\t%i" % cursor["pid"], end="")
-                if args["command_line"]:
-                    print("\t%s" % cursor["command_line"], end="")
-                print()
-
+                rec_intro(bus, service, "/", cursor)
     except:
         print("Got an exception in the main loop")
         continue
